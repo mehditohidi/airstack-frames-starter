6b904a255df25d361cf9a8a6634333b0c6cf2432
ThEmEhT <57602589+mehditohidi@users.noreply.github.com>
2024-04-18 19:24:50 +0330

    farcast

--- api/index.tsx
+++ api/index.tsx
@@ -120,7 +120,11 @@ app.frame("/", async (c) => {
         style={{
           color: "white",
           display: "flex",
-
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          height: "100%",
          
           
         }} 
